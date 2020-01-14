44dc14d9d57d3d7d1017238ea72ef0a10b609295
Shou Matsumoto <smatsum2@gmu.edu>
2020-01-14 12:20:37 -0500

    Removed a TODO comment because it was addressed in git history
    Git history has shown enough evidence to indicate that Meta package was removed on purpose (this settles this TODO comment).

--- Core/__init__.py
+++ Core/__init__.py
@@ -1,9 +1,6 @@
 """ This package declares meta data for SAI schema.
 """
 
-# Meta can't be accessed from here... TODO verify with Young
-# from Meta import *
-
 # Force __all__ modules to be imported when importing Core package
 from Core import *
 
