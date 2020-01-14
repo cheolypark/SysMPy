db9281e9ece484d62c2b8d54f305b85363cbb205
Shou Matsumoto <smatsum2@gmu.edu>
2020-01-14 12:08:20 -0500

    Package's init will now import all modules in Core
    The __init__ file in Core package was declaring import for Meta package.  This package was inaccessible. Substituted with an import to Core package.

--- Core/__init__.py
+++ Core/__init__.py
@@ -1,4 +1,11 @@
 """ This package declares meta data for SAI schema.
 """
 
-from Meta import *
+# Meta can't be accessed from here... TODO verify with Young
+# from Meta import *
+
+# Force __all__ modules to be imported when importing Core package
+from Core import *
+
+# Only these modules will be imported with "import *"
+__all__ = ["entity", "relationship"]
