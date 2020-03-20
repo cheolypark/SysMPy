aef2aa6023adbc1a3841e230fbf707ca4a323ef7
Han DaePhil <hdp@se2u.com>
2020-03-20 10:00:10 +0900

    Create makeMxgraphScript.py
    새로생성 진행 중

---
+++ code_gui/mxgraph/src/makeMxgraphScript.py
@@ -0,0 +1,12 @@
+
+
+class makeMxGraphScript():
+
+    #스타일 딕셔러리
+    style = {}
+
+    def __init__(self):
+        print("makeMxGraphScript function-----------------" )
+
+    def addSytle(self, style):
+        self.style.
