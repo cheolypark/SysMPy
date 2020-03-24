201fae4708d2c19b035adfc18f23ec5c93f101bc
Cheol Young Park <pcyoung75@gmail.com>
2020-03-24 18:11:08 -0400

    The setup file was added.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SysMPy",
+    version="0.0.1",
+    author="SysMPy Team",
+    license="Apache License Version 2.0",
+    author_email="pcyoung75@gmail.com",
+    description="A library for System Modeling Runtime Environment (SMRE)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pcyoung75/SysMPy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
