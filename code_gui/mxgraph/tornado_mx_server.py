e24d026dd2454de8572aee83a7dfc8d9673f1a21
Han DaePhil <hdp@se2u.com>
2020-03-24 10:33:16 +0900

    G2M 테스트 첫회 커밋
    한대필 G2M 테스트 첫회 커밋

--- code_gui/mxgraph/tornado_mx_server.py
+++ code_gui/mxgraph/tornado_mx_server.py
@@ -14,6 +14,52 @@ import ad_script
 import bd_script
 import hd_script
 
+from entity import *
+from code_gui.gui_mxgraph_action_diagram import *
+
+import core
+
+#sys.path.insert(0, os.path.abspath('..\..\core'))
+#sys.path.insert(0, os.path.abspath('..\..\code_gui'))
+
+# p를 다루자
+class aaa(RequestHandler):
+    def get(self):
+        my_graph = self.get_arguments("evt")
+        my_graph = str(my_graph[0])
+        my_graph = my_graph.replace("/n", "\n")
+        self.write( my_graph )
+
+
+# /ad_sample/ 핸들러
+class ActionDiagramSampleHandler(RequestHandler):
+    def get(self):
+        p = Process("Root Process")
+        p1, p2 = p.Condition("한글 컨디션 입니다. 이것은 테스트 ", "P1", "P2")
+        p_act1 = p1.Action("Action1")
+        p3, p4 = p1.Condition("Condition 2", "P3", "P4")
+        p2_act1 = p2.Action("신규액션")
+        p_act1 = p3.Action("Action2")
+        p_act2 = p4.Action("Action3")
+        p.Action("Action4")
+        p.Action("Action5")
+        gad = GuiMXGraphActionDiagram()
+        my_graph = gad.get_mxgraph( p )
+        my_graph = my_graph.replace("/n", "\n")
+        self.write(ad_script.mxGraph_start_nice_label + ad_script.mxGraph_styles + my_graph + ad_script.mxGraph_end)
+        self.render('simple_mx_web.html')
+
+
+class DiagramModifyHandler(RequestHandler):
+    def get(self):
+        my_graph = self.get_arguments("g")
+        my_graph = str(my_graph[0])
+        my_graph = my_graph.replace("/n", "\n")
+
+        self.write(ad_script.mxGraph_start_nice_label + ad_script.mxGraph_styles + my_graph + ad_script.mxGraph_end)
+        index = 'simple_mx_web.html'
+        self.render(index)
+
 
 class ActionDiagramHandler(RequestHandler):
     def get(self):
@@ -62,7 +108,10 @@ class TornadoMXServer():
                                     (r'/src/images/(.*)', StaticFileHandler, {'path': './src/images'}),
                                     (r"/AD/", ActionDiagramHandler),
                                     (r"/BD/", BlockDiagramHandler),
-                                    (r"/HD/", HierarchyDiagramHandler)
+                                    (r"/HD/", HierarchyDiagramHandler),
+                                    (r"/ad_sample/", ActionDiagramSampleHandler),
+                                    (r"/dmh/", DiagramModifyHandler),
+                                    (r"/aaa/", aaa)
                                     ] )
 
         http_server = tornado.httpserver.HTTPServer(application, max_header_size=1024 ** 3)
